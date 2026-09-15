aec12b3e7396ad3cf8d49e077d410c2a2c642184
gapmfi-ux <gapmfi@gmail.com>
2026-09-15 09:48:39 +0000

    Enable DEBUG mode in Config.js
    Changed DEBUG flag from false to true for testing.

--- Config.js
+++ Config.js
@@ -1,12 +1,10 @@
-/
 
 window.APP_CONFIG = {
-
+  // Apps Script web app exec URL (ends with /exec)
   API_URL: "https://script.google.com/macros/s/AKfycby8c9juCZUHbRtd2K5TzPI1EnNBktymKS7Nj0BWDiDzHDZIsDxvi08ksaXjGLjj3E5I/exec",
 
-  // Optional extras you may want later:
   APP_NAME: "CREDIT OPERATIONS",
   APP_VERSION: "1.0.0",
   REQUEST_TIMEOUT_MS: 30000,
-  DEBUG: false
+  DEBUG: true
 };
