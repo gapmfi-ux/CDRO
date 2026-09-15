4d0523ab444db615d1ceb83bb74fed4a51a101c4
gapmfi-ux <gapmfi@gmail.com>
2026-09-15 09:41:16 +0000

    Create Config.js

---
+++ Config.js
@@ -0,0 +1,12 @@
+/
+
+window.APP_CONFIG = {
+
+  API_URL: "https://script.google.com/macros/s/AKfycby8c9juCZUHbRtd2K5TzPI1EnNBktymKS7Nj0BWDiDzHDZIsDxvi08ksaXjGLjj3E5I/exec",
+
+  // Optional extras you may want later:
+  APP_NAME: "CREDIT OPERATIONS",
+  APP_VERSION: "1.0.0",
+  REQUEST_TIMEOUT_MS: 30000,
+  DEBUG: false
+};
